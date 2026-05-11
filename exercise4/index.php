7205c890897b8b7fb8faeeef2759e4a7bb1a1690
Meng Chomraoen <chamraoenmeng@gmail.com>
2026-05-11 15:23:16 +0700

    adding Exercise 4 till Exercise 8

---
+++ exercise4/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    h1 {
+      color: red;
+    }
+  </style>
+</head>
+
+<body>
+  <?php
+  echo "<h1>Hello PHP first</h1>";
+  echo "<h2><b>Hello PHP first</b></h2>";
+  echo "<center>Hello PHP first</center>";
+  ?>
+</body>
+
+</html>
---
+++ exercise5/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  # declare a variable
+  $a = 1000;
+  $b = 1200;
+  $c = 1400;
+
+  echo "<p><font color=blue  size='4pt'>The Salary Mr. A is: <font color=red>$a</font></font></p>";
+  echo "<p><font color=red  size='4pt'>The Salary Mr. B is: $b</font></p>";
+  echo "<p><font color=green  size='4pt'>The Salary Mr. C is: $c</font></p>"
+  ?>
+</body>
+
+</html>
