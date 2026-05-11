cfd7715d78d0a1b6894712da96303532846ae5df
Meng Chomraoen <chamraoenmeng@gmail.com>
2026-05-11 15:00:31 +0700

    Exercise1

---
+++ exercise1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+
+  // This is a single-line comment
+  # This is also a single-line comment
+
+  /*
+    weerwrwegw
+    This is a multi-line comment
+    It can span multiple lines
+  */
+
+  echo "Hello World";
+  ?>
+</body>
+
+</html>
